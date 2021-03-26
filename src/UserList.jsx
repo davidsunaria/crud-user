84d7e49f0eb694bd7ba76b44a78970bc5280a7ab
Vinesh Goyal <vinesh.kumar@nomissolutions.com>
2021-03-26 12:41:50 +0530

    clean the code

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,14 +1,11 @@
 import React from "react"
 import SingleData from "./SingleData"
 class UserList extends React.Component {
-
     render() {
-
         return (
-
             <div>
                 {this.props.userData.map((singleUser) => {
-                    return <SingleData select={"name"} key={singleUser.id} border="2px solid red" data={singleUser} />
+                    return <SingleData select={"name"} key={singleUser.id} data={singleUser} />
                 })}
             </div>
         )
